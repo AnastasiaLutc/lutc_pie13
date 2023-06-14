72b48d7dbf781df6a264095b25c05bf26511a4e4
AnastasiaLutc <lutcevan@yandex.ru>
2023-06-14 18:08:42 +0700

    второй

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          учим реакт
+          плюс разбираемся с комментами
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
