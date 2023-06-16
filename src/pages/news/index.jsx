ac7c1706471446ba8ded12c4967aeaa14b099ec3
AnastasiaLutc <lutcevan@yandex.ru>
2023-06-16 14:53:26 +0700

    закончена страница направления

--- src/pages/news/index.jsx
+++ src/pages/news/index.jsx
@@ -4,7 +4,7 @@ import "./style.scss"
 export const NewsPage = () => {
     return <div className={"orientation"}>
         <div  className={'prev'}><img src={"./image/planetary.png"} style={{width:100+"%"}} alt="Банер"/>
-            <div><p>Главные новости космоса</p></div>
+            <div><h1>Главные новости космоса</h1></div>
         </div>
         <div className={"title"}><span>Новости</span></div>
     </div>}
