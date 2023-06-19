b01da30f49d74142a54d1ecbfc7f241542817e55
AnastasiaLutc <lutcevan@yandex.ru>
2023-06-19 20:07:01 +0700

    закончена афиша

--- src/pages/news/index.jsx
+++ src/pages/news/index.jsx
@@ -7,25 +7,25 @@ export const NewsPage = () => {
             <div><h1>Главные новости космоса</h1></div>
         </div>
         <div><div className={"title"}><span>Новости</span></div>
-            <div className={"news"}>
+            <div className={"new"}>
                 <h2>Наши новости</h2>
-                <div className={"news__notes"}>
+                <div className={"new__note"}>
                     <h3>Проведено очередное заседания Научно-методического Совета № 2 от 25 мая 2023 года</h3>
                     <h4>Проведено очередное заседания Научно-методического Совета № 2 от 25 мая 2023 года На повестке дня обсуждались следующие вопросы: 1. О Международном дне астрономии: вечерние публичные наблюдения на астрономической площадке... <a className={"button"} href="https://planetarium22.ru/news/14614/" target="_blank" rel="noreferrer">Подробнее</a></h4>
                 </div>
-                <div className={"news__notes"}>
+                <div className={"new__note"}>
                     <h3>Международный День Солнца 2023</h3>
                     <h4>На астрономической площадке Планетария для всех гостей состоялся праздник «Солнечное настроение» с важным Звездочётом и его помощниками – Тучкой, Ветерком. <a className={"button"} href="https://planetarium22.ru/news/14461/" target="_blank" rel="noreferrer">Подробнее</a></h4>
                 </div>
-                <div className={"news__notes"}>
+                <div className={"new__note"}>
                     <h3>Май астрономический</h3>
                     <h4>Солнечный май вдохновляет наблюдать за активностью Солнца: его протуберанцы и темные пятна в окуляре солярного телескопа на астрономической площадке Планетария (с расписанием лучше ознакомиться заранее на официальном Сайте планетария).<a className={"button"} href="https://planetarium22.ru/news/14421/" target="_blank" rel="noreferrer">Подробнее</a></h4>
                 </div>
-                <div className={"news__notes"}>
+                <div className={"new__note"}>
                     <h3>Весенний Международный день астрономии</h3>
                     <h4>Невероятно популярны в культурно-просветительском центре «Планетарий» публичные вечерние наблюдения в телескоп, так называемый проект «Вечера тротуарной астрономии». Весной они приуроченные к Международному Дню астрономии – в этом году он отмечается 29 апреля.<a className={"button"} href="https://planetarium22.ru/news/14375/" target="_blank" rel="noreferrer">Подробнее</a></h4> 
                 </div>
-                <div className={"news__notes"} style={{marginBottom:10}}>
+                <div className={"new__note"} style={{marginBottom:10}}>
                     <h3>Международный день космонавтики 2023</h3>
                     <h4>Кому мы, жители XXI века, обязаны отдать честь за комфортную возможность – знать много интересного и увлекательного о космосе? Естественно Космонавтам… И первому космонавту – Юрию Гагарину! Космонавт ставит перед собой важные задачи! <a className={"button"} href="https://planetarium22.ru/news/14228/" target="_blank" rel="noreferrer">Подробнее</a></h4>
                 </div>
@@ -35,25 +35,25 @@ export const NewsPage = () => {
                 </div>
             </div>
             <div className={'line'}></div>
-            <div className={"news"}>
+            <div className={"new"}>
                 <h2>Новости космоса</h2>
-                <div className={"news__notes"}>
+                <div className={"new__note"}>
                     <h3>SpaceX запустила спутник Arabsat BADR-8</h3>
                     <h4>В субботнюю ночь, после недели задержек из-за штормовой погоды, SpaceX запустила спутник Arabsat BADR-8 на геосинхронную орбиту. Запуск был осуществлен с помощью ракеты Falcon 9 с мыса Канаверал.<a className={"button"} href="https://www.astronews.ru/cgi-bin/mng.cgi?page=news&news=20230529190316" target="_blank" rel="noreferrer">Подробнее</a></h4>
                 </div>
-                <div className={"news__notes"}>
+                <div className={"new__note"}>
                     <h3>Астрономы обнаружили необычную планетную систему</h3>
                     <h4>Исследователи Льежского университета и CSIC с помощью телескопа НАСА TESS обнаружили систему из двух планет немного больше Земли, которые синхронно вращаются вокруг холодной звезды.<a className={"button"} href="https://www.astronews.ru/cgi-bin/mng.cgi?page=news&news=20230528212441" target="_blank" rel="noreferrer">Подробнее</a></h4>
                 </div>
-                <div className={"news__notes"}>
+                <div className={"new__note"}>
                     <h3>НАСА и Boeing сообщили, что подготовка к июльскому испытательному полету Starliner продолжается</h3>
                     <h4>26 мая НАСА и компания Boeing заявили, что они продолжают работать над тем, чтобы запустить CST-100 Starliner в испытательный полет с экипажем в июле, несмотря на "возникающие проблемы" и опасения, высказанные комиссией<a className={"button"} href="https://www.astronews.ru/cgi-bin/mng.cgi?page=news&news=20230528122941" target="_blank" rel="noreferrer">Подробнее</a></h4>
                 </div>
-                <div className={"news__notes"}>
+                <div className={"new__note"}>
                     <h3>Миссии «Чанъэ» внесли огромный вклад в исследование Луны</h3>
                     <h4>Обзорная статья об исследовании Луны Китаем была опубликована в журнале Science China Earth Sciences 28 апреля. Все пять китайских миссий «Чанъэ» увенчались успехом. Первая миссия позволила получить некоторые базовые знания... <a className={"button"} href="https://www.astronews.ru/cgi-bin/mng.cgi?page=news&news=20230527214527" target="_blank" rel="noreferrer">Подробнее</a></h4>
                 </div>
-                <div className={"news__notes"} style={{marginBottom:10}}>
+                <div className={"new__note"} style={{marginBottom:10}}>
                     <h3>Космический аппарат LRO исследует место падения лунного модуля HAKUTO-R</h3>
                     <h4>11 декабря 2022 года был запущен лунный модуль HAKUTO-R Mission 1. Это финансируемый из частных источников космический аппарат, который планировалось посадить на поверхность Луны. После нескольких месяцев полета к Луне... <a className={"button"} href="https://www.astronews.ru/cgi-bin/mng.cgi?page=news&news=20230526202359" target="_blank" rel="noreferrer">Подробнее</a></h4>
                 </div>
--- src/pages/poster/index.jsx
+++ src/pages/poster/index.jsx
@@ -6,5 +6,77 @@ export const PosterPage = () => {
         <div  className={'prev'}><img src={"./image/planetary.png"} style={{width:100+"%"}} alt="Банер"/>
             <div><p>Путешествия по звёздным мирам</p></div>
         </div>
-        <div className={"title"}><span>Расписание</span></div>
-    </div>}
+        <div className={"schedule"}>
+            <h1>Рассписание</h1>
+            
+            <div class="dropdown">
+                <button class="dropbtn" style={{marginRight:60}}>Категория <img src={"./icons/Vector.png"} alt="vector"/></button>
+                <div class="dropdown-content">
+                <a href="/poster">0+</a>
+                <a href="/poster">6+</a>
+                <a href="/poster">12+</a>
+                <a href="/poster">16+</a>
+                </div>
+            </div>
+            <div class="dropdown" style={{marginRight:60}}>
+                <button class="dropbtn" style={{width:320}}>Место проведения <img src={"./icons/Vector.png"} alt="vector"/></button>
+                <div class="dropdown-content">
+                <a href="/poster">Зал мультимедиа</a>
+                <a href="/poster">Площадка у планетария</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Направление <img src={"./icons/Vector.png"} alt="vector"/></button>
+                <div class="dropdown-content">
+                <a href="/poster">Полнокупольные шоу-программы</a>
+                <a href="/poster">Астрономические наблюдения</a>
+                <a href="/poster">Мультимедийные программы</a>
+                </div>
+            </div>
+            <a className={"button"} href="/poster">Применить</a>
+            <a className={"button"} href="/poster" style={{left:-255}}>Сбросить</a>
+            
+        </div>
+        <div className={"poster"}> 
+            <div className={"conteiner_p"} style={{marginRight:23}}>
+                <div className={"arrow_p left"}><div></div></div>
+            </div>
+            <div>
+                <div className={'frame_data'}><p>29 Мая, Вт</p></div>
+                <div className={'frame_time'}><h5>11.00 - 11.50</h5><h6>Полнокупольные шоу-программы</h6></div>
+                <div className={'frame_time'}><h5>12.00 - 13.10</h5><h6>Полнокупольные шоу-программы</h6></div>
+                <div className={'frame_time'}><h5>16.00 - 16.50</h5><h6>Полнокупольные шоу-программы</h6></div>
+            </div>
+            <div>
+                <div className={'frame_data'}><p>30 Мая, Ср</p></div>
+                <div className={'frame_time'}><h5>12.00 - 13.10</h5><h6 style={{color:'#7000A5'}}>Астрономические наблюдения</h6></div>
+                <div className={'frame_time'}><h5>14.00 - 14.40</h5><h6>Полнокупольные шоу-программы</h6></div>
+            </div>
+            <div>
+                <div className={'frame_data'}><p>31 Мая, Чт</p></div>
+                <div className={'frame_time'}><h5>11.00 - 11.50</h5><h6>Полнокупольные шоу-программы</h6></div>
+            </div>
+            <div>
+                <div className={'frame_data'}><p>1 Июня, Пт</p></div>
+                <div className={'frame_time'}><h5>14.00 - 14.40</h5><h6>Полнокупольные шоу-программы</h6></div>
+                <div className={'frame_time'}><h5>16.00 - 16.50</h5><h6>Полнокупольные шоу-программы</h6></div>
+            </div>
+            <div>
+                <div className={'frame_data'}><p>2 Июня, Сб</p></div>
+                <div className={'frame_time'}><h5>11.00 - 11.50</h5><h6>Полнокупольные шоу-программы</h6></div>
+                <div className={'frame_time'}><h5>12.00 - 13.10</h5><h6>Полнокупольные шоу-программы</h6></div>
+                <div className={'frame_time'}><h5>14.00 - 14.40</h5><h6>Полнокупольные шоу-программы</h6></div>
+                <div className={'frame_time'}><h5>20.00</h5><h6 style={{color:'#7000A5'}}>Астрономические наблюдения</h6></div>
+            </div>
+            <div>
+                <div className={'frame_data'}><p>3 Июня, Вс</p></div>
+                <div className={'frame_time'}><h5>11.00 - 11.50</h5><h6>Полнокупольные шоу-программы</h6></div>
+                <div className={'frame_time'}><h5>14.00 - 14.40</h5><h6>Полнокупольные шоу-программы</h6></div>
+                <div className={'frame_time'}><h5>16.00 - 16.50</h5><h6>Полнокупольные шоу-программы</h6></div>
+            </div>
+            <div className={"conteiner_p"} style={{marginLeft:23}}>
+                <div className={"arrow_p right"}><div></div></div>
+            </div>
+        </div>
+    </div>
+}
