05eb7804a1af10e79b1a938a282b3d66663170e8
AnastasiaLutc <lutcevan@yandex.ru>
2023-06-22 16:35:51 +0700

    footer mobile 840px

--- src/pages/footer/index.jsx
+++ src/pages/footer/index.jsx
@@ -4,14 +4,14 @@ import './style.scss'
 export const FooterPage = () => {
     return <div className={'footer'}>
         <div className={'footer__contacts'}> 
-        <p className={'header_text'}>Контакты</p>
-        <p className={'text'}>Адрес: Алтайский край, город Барнаул, пр. Ленина, 19 <br /> Телефоны: +7 (3852) 61-10-65 +7 (3852) 50-06-43 <br/>
+            <p className={'header_text'}>Контакты</p>
+            <p className={'text'}>Адрес: Алтайский край, город Барнаул, пр. Ленина, 19 <br /> Телефоны: +7 (3852) 61-10-65 +7 (3852) 50-06-43 <br/>
                     E-mail: planetary@list.ru</p>
-         </div>
+        </div>
         <div className={'footer__data'}>
-        <div><img src={'./picture/telegram.svg'} style={{marginRight:10}} alt="icon_telegram"/>
-        <img src={'./picture/vk.svg'} alt="icon_vk"/></div>
-        <div><p>© 2023 github</p></div>
+            <div><img src={'./picture/telegram.svg'} style={{marginRight:10}} alt="icon_telegram"/>
+            <img src={'./picture/vk.svg'} alt="icon_vk"/></div>
+            <div><p>© 2023 github</p></div>
         </div>
     </div>
 }
